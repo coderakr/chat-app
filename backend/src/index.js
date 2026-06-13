89b2b37179d85b8feb83cdb4391aee81c003b102
coderakr <139627558+coderakr@users.noreply.github.com>
2026-06-13 11:21:39 +0530

    webhooks added

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,19 @@ import path from "path";
 
 import "dotenv/config";
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use(
+  "api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
